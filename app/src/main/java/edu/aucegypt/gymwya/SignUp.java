f54c347dcf3cdafc32e72bb3ef18da88cf880d5a
Youssef Rami <yousseframi@aucegypt.edu>
2023-07-15 22:54:57 +0300

    fixed minor bugs

--- app/src/main/java/edu/aucegypt/gymwya/SignUp.java
+++ app/src/main/java/edu/aucegypt/gymwya/SignUp.java
@@ -22,71 +22,62 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class SignUp extends AppCompatActivity {
-        Button createAccount;
-        EditText email, password, rePassword;
-        ImageView back;
+    Button createAccount;
+    EditText email, password, rePassword;
+    ImageView back;
 
-        @SuppressLint("MissingInflatedId")
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.sign_up);
-                FirebaseApp.initializeApp(this);
-                createAccount = (Button) findViewById(R.id.create_account);
-                // googleSignUp = (Button) findViewById(R.id.continueWithGoogle);
-                email = (EditText) findViewById(R.id.userEmail);
-                password = (EditText) findViewById(R.id.enteredPassword);
-                rePassword = (EditText) findViewById(R.id.reEnteredPassword);
-                back = findViewById(R.id.back);
-                FirebaseFirestore db = FirebaseFirestore.getInstance();
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+        FirebaseApp.initializeApp(this);
+        createAccount = (Button) findViewById(R.id.create_account);
+        // googleSignUp = (Button) findViewById(R.id.continueWithGoogle);
+        email = (EditText) findViewById(R.id.userEmail);
+        password = (EditText) findViewById(R.id.enteredPassword);
+        rePassword = (EditText) findViewById(R.id.reEnteredPassword);
+        back = findViewById(R.id.back);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-                createAccount.setOnClickListener(view -> {
-                        AtomicReference<Boolean> databaseError = new AtomicReference<>(false);
+        createAccount.setOnClickListener(view -> {
+            AtomicReference<Boolean> databaseError = new AtomicReference<>(false);
 
-                        // add check for email and password to see if they are empty or not and if they
-                        // are empty then send error message
-                        if (!password.getText().toString().equals(rePassword.getText().toString())) {
-                                Toast.makeText(SignUp.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+            // add check for email and password to see if they are empty or not and if they
+            // are empty then send error message
+            if (!password.getText().toString().equals(rePassword.getText().toString())) {
+                Toast.makeText(SignUp.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
 
-                        } else if (password.getText().toString().length() < 6) {
-                                Toast.makeText(SignUp.this, "Password must be at least 6 characters",
-                                                Toast.LENGTH_SHORT).show();
-                        } else if (email.getText().toString().isEmpty() && password.getText().toString().isEmpty()
-                                        && rePassword.getText().toString().isEmpty()) {
-                                Toast.makeText(SignUp.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
-                        } else {
-
-                                FirebaseAuth.getInstance()
-                                                .createUserWithEmailAndPassword(email.getText().toString(),
-                                                                password.getText().toString())
-                                                .addOnCompleteListener(task -> {
-                                                        if (task.isSuccessful()) {
-                                                                Toast.makeText(SignUp.this, "User Created",
-                                                                                Toast.LENGTH_SHORT).show();
-                                                        } else {
-                                                                Toast.makeText(SignUp.this,
-                                                                                "Error ! " + task.getException()
-                                                                                                .getMessage(),
-                                                                                Toast.LENGTH_SHORT).show();
-                                                        }
-                                                        if (task.isSuccessful()) {
-                                                                Intent intent = new Intent(SignUp.this, CreateUser.class);
-                                                                intent.putExtra("password", password.getText().toString());
-                                                                intent.putExtra("email", email.getText().toString());
-                                                                startActivity(intent);
-                                                        }                                                });
-
-
-                        }
-
-
-                });
+            } else if (password.getText().toString().length() < 6) {
+                Toast.makeText(SignUp.this, "Password must be at least 6 characters",
+                        Toast.LENGTH_SHORT).show();
+            } else if (email.getText().toString().isEmpty() && password.getText().toString().isEmpty()
+                    && rePassword.getText().toString().isEmpty()) {
+                Toast.makeText(SignUp.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+            } else {
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(SignUp.this, "User Created",
+                                Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(SignUp.this,
+                                "Error ! " + task.getException()
+                                        .getMessage(),
+                                Toast.LENGTH_SHORT).show();
+                    } if (task.isSuccessful()) {
+                        Intent intent = new Intent(SignUp.this, CreateUser.class);
+                        intent.putExtra("password", password.getText().toString());
+                        intent.putExtra("email", email.getText().toString());
+                        startActivity(intent);
+                    }});
+            }
+        });
 
 
 
-                back.setOnClickListener(v -> {
-                        finish();
-                });
+        back.setOnClickListener(v -> {
+            finish();
+        });
 
-        }
+    }
 }
